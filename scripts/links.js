190e61f02a9cb108d108905654d1e7def46d5011
Vicente <vicenteferrer_01@hotmail.com>
2024-03-06 17:13:56 -0300

    W09 JSON Link List Implemented

--- scripts/links.js
+++ scripts/links.js
@@ -1,11 +1,53 @@
-const baseURL = "https://vicentemferrer.github.io/wdd230/"
 const linksURL = "https://vicentemferrer.github.io/wdd230/data/links.json"
+
+const linkComponent = ({ url, title }) => {
+  const linkElement = document.createElement('a')
+  linkElement.setAttribute('href', url)
+
+  if (/codepen/.test(url) || !/html/.test(url)) linkElement.setAttribute('target', '_blank')
+
+  linkElement.textContent = title
+
+  return linkElement
+}
+
+const spanElement = () => {
+  const spanTag = document.createElement('span')
+  spanTag.setAttribute('class', 'divider')
+  spanTag.textContent = '|'
+  return spanTag
+}
+
+const lessonComponent = ({ lesson, links }) => {
+  const listItemElement = document.createElement('li')
+  const linksComposed = links.map(link => linkComponent(link))
+
+  listItemElement.innerHTML = `Week ${lesson}: `
+
+  linksComposed.forEach((link, index, array) => {
+    listItemElement.appendChild(link)
+
+    if (index !== array.length - 1) listItemElement.appendChild(spanElement())
+  })
+
+  return listItemElement
+}
+
+function displayLinks({ lessons }) {
+  const lessonList = document.querySelector('section.card ul')
+
+  lessons.forEach(lesson => {
+    const lessonElement = lessonComponent(lesson)
+
+    lessonList.appendChild(lessonElement)
+  })
+}
 
 async function getLinks(url) {
   const response = await fetch(url)
   const data = await response.json()
 
-  return data
+  displayLinks(data)
 }
 
-console.log(await getLinks(linksURL))
+getLinks(linksURL)
